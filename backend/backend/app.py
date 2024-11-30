69947f99b137e0c45014d8305e45624d2e0a903f
Juliana Zacharias <julianazacharias.dev@gmail.com>
2024-11-30 01:29:53 -0300

    Initial backend structure

---
+++ backend/backend/app.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from backend.config.settings import Settings
+from backend.routers import auth, task, user
+
+settings = Settings()
+
+app = FastAPI()
+
+origins = [settings.FRONTEND_URL]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=[
+        '*'
+    ],  # ["Origin", "Content-Type", "Accept", "Authorization"]
+)
+
+app.include_router(user.router)
+app.include_router(auth.router)
+app.include_router(task.router)
+
+
+@app.get('/')
+def read_root():
+    return {'message': 'Hello World!'}
