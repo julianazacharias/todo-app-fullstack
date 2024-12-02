eebb2017276698f78845d14576173358ad9be793
Juliana Zacharias <julianazacharias.dev@gmail.com>
2024-12-02 01:10:47 -0300

    🔧 refactor: color and layout changes in navbar and hero section

--- frontend/src/Components/Hero/Hero.tsx
+++ frontend/src/Components/Hero/Hero.tsx
@@ -6,20 +6,20 @@ const Hero = (props: Props) => {
 	return (
 		<section
 			id="hero"
-			className="h-screen w-full bg-gradient-to-r from-pink-300 via-teal-250 to-orange-200 flex items-center"
+			className="h-screen w-full bg-gradient-to-r from-indigo-950 via-teal-250 to-purple-800 flex items-center"
 		>
-			<div className="container mx-auto flex flex-col-reverse lg:flex-row items-center px-8 lg:px-28 h-full">
+			<div className="container mx-auto flex flex-col-reverse lg:flex-row items-center  px-8 lg:px-28 h-full">
 				<div className="flex flex-col space-y-10 mb-20 lg:w-1/2 h-full align-middle justify-center text-center lg:text-left">
-					<h1 className="text-3xl lg:text-5xl font-extrabold text-slate-700 text-wrap leading-tight">
+					<h1 className="text-3xl lg:text-5xl font-extrabold text-purple-300 text-wrap leading-tight">
 						My TODO List
 					</h1>
-					<p className="text-lg lg:text-xl text-slate-700">
+					<p className="text-lg lg:text-xl text-white">
 						Organize your tasks and stay productive with ease
 					</p>
 					<div className="mx-auto lg:mx-0">
 						<Link
 							to="/login"
-							className="py-3 px-6 text-lg font-semibold text-slate-700 bg-gradient-to-r from-cyan-600 to-teal-400  rounded-lg hover:shadow-xl transition-shadow duration-300 shadow-md"
+							className="py-3 px-6 text-lg font-semibold text-white bg-gradient-to-r from-indigo-600 to-purple-600  rounded-lg hover:shadow-xl transition-shadow duration-300 shadow-md"
 						>
 							Get Started
 						</Link>
--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -9,14 +9,14 @@ const Navbar = (props: Props) => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
 
 	return (
-		<nav className="relative w-full p-4 bg-gradient-to-r from-pink-300 via-teal-250 to-orange-200">
+		<nav className="relative w-full p-4 bg-gradient-to-r from-indigo-950 via-teal-250 to-purple-800">
 			<div className="flex items-center justify-between w-full py-4">
 				{/* Logo and Navigation Links */}
 				<div className="flex items-center space-x-6">
 					<div className="font-bold">
 						<Link
 							to="/"
-							className="text-slate-700 text-3xl hover:opacity-80 transition-opacity duration-300"
+							className="text-purple-300 text-3xl hover:opacity-80 transition-opacity duration-300"
 						>
 							TODO List
 						</Link>
@@ -37,36 +37,36 @@ const Navbar = (props: Props) => {
 				<div className="hidden lg:flex lg:items-center text-gray-900">
 					{/* Conditional Rendering for Authenticated User */}
 					{isLoggedIn() ? (
-						<div className="flex items-center space-x-6 text-gray-900">
-							<div className="text-xl font-semibold hover:text-teal-600 transition-all duration-300">
-								Welcome, {user?.username}
+						<div className="flex items-center space-x-6 text-purple-300">
+							<div className="text-xl font-semibold hover:opacity-80 transition-opacity duration-300">
+								{user?.username}
 							</div>
-							<div className="flex items-center space-x-4 font-bold text-gray-900">
+							<div className="flex items-center space-x-4 font-bold text-white">
 								<Link
 									to="/tasks"
-									className="px-8 py-3 font-bold rounded-full text-slate-700 bg-gradient-to-r from-violet-500 to-fuchsia-400 hover:opacity-80 transition-opacity duration-300"
+									className="px-8 py-3 font-bold rounded-full bg-gradient-to-r from-indigo-500 to-violet-500 hover:opacity-80 transition-opacity duration-300"
 								>
 									My List
 								</Link>
 								<a
 									onClick={logout}
-									className="px-8 py-3 font-bold rounded-full bg-gradient-to-r from-orange-500 to-amber-400 hover:opacity-80 transition-opacity duration-300"
+									className="px-8 py-3 font-bold rounded-full bg-gradient-to-r from-slate-900 to-slate-600 hover:opacity-80 transition-opacity duration-300"
 								>
 									Logout
 								</a>
 							</div>
 						</div>
 					) : (
-						<div className="flex items-center space-x-6 font-bold text-gray-900">
+						<div className="flex items-center space-x-6 font-bold text-white">
 							<Link
 								to="/login"
-								className="hover:text-teal-600 transition-all duration-300"
+								className="hover:text-teal-400 transition-all duration-300"
 							>
 								Login
 							</Link>
 							<Link
 								to="/register"
-								className="px-8 py-3 font-bold rounded-full text-slate-700 bg-gradient-to-r from-cyan-600 to-teal-400 hover:opacity-80 transition-opacity duration-300"
+								className="px-8 py-3 font-bold rounded-full bg-gradient-to-r from-cyan-800 to-teal-500 hover:opacity-80 transition-opacity duration-300"
 							>
 								Sign Up
 							</Link>
@@ -84,36 +84,36 @@ const Navbar = (props: Props) => {
 				{/* Conditional Rendering for Authenticated User */}
 				{isLoggedIn() ? (
 					<div className="flex justify-center items-center w-full h-full">
-						<div className="flex flex-col px-3 w-1/3 font-semibold">
-							Welcome, {user?.username}
+						<div className="flex flex-col px-3 w-1/3 font-semibold text-purple-300">
+							<div>{user?.username}</div>
 						</div>
-						<div className="space-x-3">
+						<div className="space-x-3 text-white">
 							<Link
 								to="/tasks"
-								className="px-6 py-2 font-bold text-sm rounded-full text-slate-700 bg-gradient-to-r from-violet-500 to-fuchsia-400 hover:opacity-80 transition-opacity duration-300"
+								className="px-6 py-2 font-bold text-sm rounded-full  bg-gradient-to-r from-indigo-500 to-violet-500 hover:opacity-80 transition-opacity duration-300"
 							>
 								My List
 							</Link>
 							<a
 								onClick={logout}
-								className="px-6 py-2 font-bold text-sm rounded-full text-slate-700 bg-gradient-to-r from-orange-500 to-amber-400 hover:opacity-80 transition-opacity duration-300"
+								className="px-6 py-2 font-bold text-sm rounded-full  bg-gradient-to-r from-slate-900 to-slate-600  hover:opacity-80 transition-opacity duration-300"
 							>
 								Logout
 							</a>
 						</div>
 					</div>
 				) : (
-					<div className="flex justify-center items-center w-full h-full">
+					<div className="flex justify-center items-center w-full h-full text-white">
 						<div className="flex flex-col space-y-4 w-1/3">
 							<Link
 								to="/login"
-								className="px-6 py-2 font-bold text-sm rounded-full text-slate-700 bg-gradient-to-r from-pink-500 to-rose-300 hover:opacity-80 transition-opacity duration-300"
+								className="px-6 py-2 font-bold text-sm rounded-full bg-gradient-to-r from-pink-800 to-rose-500 hover:opacity-80 transition-opacity duration-300"
 							>
 								Login
 							</Link>
 							<Link
 								to="/register"
-								className="px-6 py-2 font-bold text-sm rounded-full text-slate-700 bg-gradient-to-r from-cyan-600 to-teal-400 hover:opacity-80 transition-opacity duration-300"
+								className="px-6 py-2 font-bold text-sm rounded-full  bg-gradient-to-r from-cyan-800 to-teal-500  hover:opacity-80 transition-opacity duration-300"
 							>
 								Sign Up
 							</Link>
