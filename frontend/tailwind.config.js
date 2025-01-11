55c90422e27be7030c2bb347141bac585680a3e9
Juliana Zacharias <julianazacharias.dev@gmail.com>
2025-01-11 15:35:41 -0300

    🐞 fix: testing tailwing config for deploy

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-		"./src/**/*.{html,js,jsx,ts,tsx}", // Ensure this matches your file paths
-	],
+	content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
 	theme: {
-		extend: {
-			fontFamily: {
-				inter: ["Inter", "sans-serif"],
-			},
-		},
+		extend: {},
 	},
 	plugins: [],
 };
